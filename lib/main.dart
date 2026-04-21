a49ce3b12d552d9f8235eb2696b1c0a45b591e07
hlakhaled <hlakhaled647@gmail.com>
2026-04-21 17:57:01 +0200

    refactor: simplify widget structure in related device card and update API service timeout settings

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
-
 import 'Features/overview/notifications/data/repos/notifications_repository.dart';
 import 'Features/overview/notifications/presentation/manager/notifications_cubit.dart';
 
