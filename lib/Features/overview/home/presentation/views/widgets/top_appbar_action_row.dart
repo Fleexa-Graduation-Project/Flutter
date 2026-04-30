80addf070cc046b09e43d808e57d783eb1743ffd
NourhanOElsayed <nourhanusama1602@gmail.com>
2026-04-30 18:52:58 +0300

    refactor: remove unused flutter_screenutil import from TopAppbarActionRow

--- lib/Features/overview/home/presentation/views/widgets/top_appbar_action_row.dart
+++ lib/Features/overview/home/presentation/views/widgets/top_appbar_action_row.dart
@@ -1,7 +1,6 @@
 import 'package:fleexa/Features/overview/notifications/presentation/views/widgets/notification_bell_badge.dart';
 import 'package:fleexa/core/router/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hotspot/hotspot.dart';
