5575170058a758f03486ca248b9589376f8d9945
hlakhaled <hlakhaled647@gmail.com>
2026-05-11 13:38:11 +0300

    feat: add navigation to AC control in RelatedDeviceCard and clean up SettingsView

--- lib/Features/devices/sensors/temperature/presentation/views/widgets/related_device_card.dart
+++ lib/Features/devices/sensors/temperature/presentation/views/widgets/related_device_card.dart
@@ -1,3 +1,4 @@
+import 'package:fleexa/core/router/app_router.dart';
 import 'package:fleexa/core/utils/common_widgets/custom_container.dart';
 import 'package:fleexa/core/utils/constants/app_colors.dart';
 import 'package:fleexa/core/utils/constants/assets.dart';
@@ -6,6 +7,7 @@ import 'package:fleexa/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class RelatedDeviceCard extends StatelessWidget {
   const RelatedDeviceCard({super.key});
@@ -20,27 +22,32 @@ class RelatedDeviceCard extends StatelessWidget {
           style: Styles.style18Medium,
         ),
         const SizedBox(height: 20),
-        CustomContainer(
-            child: Row(
-          children: [
-            SvgPicture.asset(
-              AppAssets.iconsAc,
-            ),
-            const SizedBox(
-              width: 16,
-            ),
-            Text(
-              S.of(context).airConditioner,
-              style: Styles.style14Medium.copyWith(color: AppColors.white),
-            ),
-            const SizedBox(height: 4),
-            const Spacer(),
-            const Icon(
-              FontAwesomeIcons.angleRight,
-              color: AppColors.mediumGray,
-            )
-          ],
-        ))
+        GestureDetector(
+          onTap: () {
+            GoRouter.of(context).pushNamed(AppRouter.acControl);
+          },
+          child: CustomContainer(
+              child: Row(
+            children: [
+              SvgPicture.asset(
+                AppAssets.iconsAc,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Text(
+                S.of(context).airConditioner,
+                style: Styles.style14Medium.copyWith(color: AppColors.white),
+              ),
+              const SizedBox(height: 4),
+              const Spacer(),
+              const Icon(
+                FontAwesomeIcons.angleRight,
+                color: AppColors.mediumGray,
+              )
+            ],
+          )),
+        )
       ],
     );
   }
