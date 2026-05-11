264695f28e1adbf36adc8d5beabe98be8fef1980
NourhanOElsayed <nourhanusama1602@gmail.com>
2026-05-11 15:29:36 +0300

    feat: update API endpoints to include versioning and correct development URL

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -8,7 +8,7 @@ class ApiConstants {
       case AppEnvironment.mock:
         return 'https://a942c4cd-805c-44de-af59-f61d99e769ac.mock.pstmn.io/api/v1';
       case AppEnvironment.dev:
-        return 'https://23wqm3dp5c.execute-api.us-east-1.amazonaws.com/dev';
+        return 'https://9qqg9f27h7.execute-api.us-east-1.amazonaws.com';
       case AppEnvironment.prod:
         return 'https://api.fleexa.com/api/v1';
     }
@@ -18,19 +18,19 @@ class ApiConstants {
   static const String awsApiKey = 'YOUR_AWS_API_KEY_HERE';
 
   // System & Device Endpoints
-  static const String systemOverview = "/system/overview";
-  static const String devices = "/devices";
-  static const String allAlerts = "/alerts";
-  static String deviceDetails(String id) => "/devices/$id";
-  static String deviceTelemetry(String id) => "/devices/$id/telemetry";
-  static String deviceAlerts(String id) => "/devices/$id/alerts";
-  static String deviceCommands(String id) => "/devices/$id/commands";
+  static const String systemOverview = "/api/v1/system/overview";
+  static const String devices = "/api/v1/devices";
+  static const String allAlerts = "/api/v1/alerts";
+  static String deviceDetails(String id) => "/api/v1/devices/$id";
+  static String deviceTelemetry(String id) => "/api/v1/devices/$id/telemetry";
+  static String deviceAlerts(String id) => "/api/v1/devices/$id/alerts";
+  static String deviceCommands(String id) => "/api/v1/devices/$id/commands";
 
   // Authentication Endpoints
-  static const String signIn = "/auth/signin";
-  static const String signUp = "/auth/signup";
-  static const String forgotPassword = "/auth/forgot-password";
-  static const String resetPassword = "/auth/reset-password";
-  static const String changePassword = "/auth/change-password";
-  static const String profile = "/auth/profile";
+  static const String signIn = "/api/v1/auth/signin";
+  static const String signUp = "/api/v1/auth/signup";
+  static const String forgotPassword = "/api/v1/auth/forgot-password";
+  static const String resetPassword = "/api/v1/auth/reset-password";
+  static const String changePassword = "/api/v1/auth/change-password";
+  static const String profile = "/api/v1/auth/profile";
 }
