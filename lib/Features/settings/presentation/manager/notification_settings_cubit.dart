53397e80d795555cb98e6b3694dd11ad93702a9d
NourhanOElsayed <nourhanusama1602@gmail.com>
2026-06-15 17:05:03 +0300

    refactor:  improved preference handling and device registration

--- lib/Features/settings/presentation/manager/notification_settings_cubit.dart
+++ lib/Features/settings/presentation/manager/notification_settings_cubit.dart
@@ -3,13 +3,12 @@ import 'dart:developer';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../core/services/push_notification_service.dart';
-import '../../../../core/utils/constants/app_strings.dart';
 import 'notification_settings_state.dart';
 
 class NotificationSettingsCubit extends Cubit<NotificationSettingsState> {
-  final PushNotificationService _notificationService =
-      PushNotificationService();
-  NotificationSettingsCubit() : super(NotificationSettingsState());
+  final PushNotificationService _notificationService;
+  NotificationSettingsCubit(this._notificationService)
+      : super(NotificationSettingsState());
 
   // 1. load Settings from SharedPreferences when the cubit is initialized
   Future<void> loadSettings() async {
@@ -19,28 +18,17 @@ class NotificationSettingsCubit extends Cubit<NotificationSettingsState> {
       criticalAlerts: prefs.getBool('criticalAlerts') ?? true,
       warningAlerts: prefs.getBool('warningAlerts') ?? true,
     ));
-    await syncSubscriptions(AppConstants.notificationDevices);
+    await syncSubscriptions();
   }
 
   // 2. sync subscriptions with firebase
-  Future<void> syncSubscriptions(List<String> userDeviceIds) async {
-    log("🔄 Starting syncSubscriptions for ${userDeviceIds.length} devices...");
-    for (String deviceId in userDeviceIds) {
-      log("Trying to sync device: $deviceId");
-      // critical channel
-      if (state.isPushEnabled && state.criticalAlerts) {
-        await _notificationService.subscribeToTopic('${deviceId}_CRITICAL');
-      } else {
-        await _notificationService.unsubscribeFromTopic('${deviceId}_CRITICAL');
-      }
-
-      // warning channel
-      if (state.isPushEnabled && state.warningAlerts) {
-        await _notificationService.subscribeToTopic('${deviceId}_WARNING');
-      } else {
-        await _notificationService.unsubscribeFromTopic('${deviceId}_WARNING');
-      }
-    }
+  Future<void> syncSubscriptions() async {
+    log("Syncing preferences with backend...");
+    await _notificationService.updatePreferences(
+      isPushEnabled: state.isPushEnabled,
+      criticalAlerts: state.criticalAlerts,
+      warningAlerts: state.warningAlerts,
+    );
   }
 
   // 3. toggle Master Push Notifications
@@ -50,7 +38,7 @@ class NotificationSettingsCubit extends Cubit<NotificationSettingsState> {
     await prefs.setBool('isPushEnabled', value);
     emit(state.copyWith(isPushEnabled: value));
 
-    await syncSubscriptions(userDeviceIds);
+    await syncSubscriptions();
   }
 
   // 3. toggle Critical Alerts
@@ -60,7 +48,7 @@ class NotificationSettingsCubit extends Cubit<NotificationSettingsState> {
     await prefs.setBool('criticalAlerts', value);
     emit(state.copyWith(criticalAlerts: value));
 
-    await syncSubscriptions(userDeviceIds);
+    await syncSubscriptions();
   }
 
   // 4. toggle Warning Alerts
@@ -70,6 +58,6 @@ class NotificationSettingsCubit extends Cubit<NotificationSettingsState> {
     await prefs.setBool('warningAlerts', value);
     emit(state.copyWith(warningAlerts: value));
 
-    await syncSubscriptions(userDeviceIds);
+    await syncSubscriptions();
   }
 }
--- lib/core/services/push_notification_service.dart
+++ lib/core/services/push_notification_service.dart
@@ -1,9 +1,13 @@
 import 'dart:developer';
+import '../network/api_constants.dart';
+import '../network/api_service.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../utils/constants/app_colors.dart';
+import '../utils/functions/device_helpers.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -15,6 +19,10 @@ class PushNotificationService {
   final _fcm = FirebaseMessaging.instance;
   static final FlutterLocalNotificationsPlugin _localNotifications =
       FlutterLocalNotificationsPlugin();
+
+  final APiService apiService;
+
+  PushNotificationService(this.apiService);
 
   Future<void> init() async {
     const AndroidInitializationSettings androidInitSettings =
@@ -63,25 +71,101 @@ class PushNotificationService {
 
     // 4. Receive notifications when the app is in the background or terminated
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    // 5. Handle token refresh
+    _fcm.onTokenRefresh.listen((newToken) async {
+      log("FCM Token refreshed by Firebase: $newToken");
+      await _updateTokenOnBackend(newToken);
+    });
   }
 
-  // 5. Subscribe to topics (e.g., device-specific topics for targeted notifications)
-  Future<void> subscribeToTopic(String topic) async {
+  // register the new token with the backend
+  Future<void> registerDeviceForNotifications() async {
     try {
-      await _fcm.subscribeToTopic(topic);
-      log("Subscribed to FCM topic: $topic");
+      final String hardwareId = await DeviceHelpers.getUniqueHardwareId();
+      final String? fcmToken = await _fcm.getToken();
+
+      if (fcmToken == null) {
+        log("FCM Token is null, cannot register device.");
+        return;
+      }
+
+      await apiService.put(
+        ApiConstants.userPreferences,
+        data: {
+          'hardware_id': hardwareId,
+          'fcm_token': fcmToken,
+          'receive_notifications': true,
+          'receive_critical': true,
+          'receive_warnings': true
+        },
+      );
+
+      log("Device [$hardwareId] registered for push notifications successfully!");
     } catch (e) {
-      log("Failed to subscribe to topic $topic: $e");
+      log("Failed to register device: $e");
     }
   }
 
-  // 6. Unsubscribe from topics (Called when the user disables Master Push Notifications)
-  Future<void> unsubscribeFromTopic(String topic) async {
+  // unregister the device
+  Future<void> unregisterDevice() async {
     try {
-      await _fcm.unsubscribeFromTopic(topic);
-      log("Unsubscribed from FCM topic: $topic");
+      final String hardwareId = await DeviceHelpers.getUniqueHardwareId();
+
+      await apiService.put(
+        ApiConstants.userPreferences,
+        data: {
+          'hardware_id': hardwareId,
+          'fcm_token':
+              "", // sends empty string to delete this device notifications.
+        },
+      );
+
+      log("Device [$hardwareId] unregistered successfully!");
     } catch (e) {
-      log("Failed to unsubscribe from topic $topic: $e");
+      log("Failed to unregister device: $e");
+    }
+  }
+
+  // update the token on the backend when it changes
+  Future<void> _updateTokenOnBackend(String newToken) async {
+    try {
+      final String hardwareId = await DeviceHelpers.getUniqueHardwareId();
+      await apiService.put(
+        ApiConstants.userPreferences,
+        data: {
+          'hardware_id': hardwareId,
+          'fcm_token': newToken,
+        },
+      );
+      log("FCM Token updated on backend for device: $hardwareId");
+    } catch (e) {
+      log("Failed to update token on backend: $e");
+    }
+  }
+
+  Future<void> updatePreferences({
+    required bool isPushEnabled,
+    required bool criticalAlerts,
+    required bool warningAlerts,
+  }) async {
+    try {
+      final String hardwareId = await DeviceHelpers.getUniqueHardwareId();
+      final String? fcmToken = await _fcm.getToken();
+
+      await apiService.put(
+        ApiConstants.userPreferences,
+        data: {
+          'hardware_id': hardwareId,
+          'fcm_token': fcmToken ?? "",
+          'receive_notifications': isPushEnabled,
+          'receive_critical': criticalAlerts,
+          'receive_warnings': warningAlerts,
+        },
+      );
+      log("Notification preferences updated successfully on backend!");
+    } catch (e) {
+      log("Failed to update preferences: $e");
     }
   }
 
